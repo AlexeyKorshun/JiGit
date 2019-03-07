57aed71d947aee4fe885c29fd9357fbb3d0892a6
Alexey Korshun <lehakorshun@gmail.com>
2019-03-07 15:42:53 +0600

    create database consts

---
+++ jigitbl/src/commonMain/kotlin/com/rosberry/mpp/jigitbl/data/DatabaseConst.kt
@@ -0,0 +1,15 @@
+/*
+ *
+ *  * Copyright (c) 2018 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.mpp.jigitbl.data
+
+/**
+ * @author Alexei Korshun on 07/03/2019.
+ */
+object DatabaseConst {
+
+    const val DATABASE_NAME = "jigitdatabase.db"
+}
