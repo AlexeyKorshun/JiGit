ca8d7940f9b4a748e29c371ee873d8755b5aeaeb
Alexey Korshun <lehakorshun@gmail.com>
2019-03-07 15:43:14 +0600

    create repository db class

---
+++ jigitbl/src/commonMain/kotlin/com/rosberry/mpp/jigitbl/data/repositories/RepositoryDb.kt
@@ -0,0 +1,41 @@
+/*
+ *
+ *  * Copyright (c) 2018 Rosberry. All rights reserved.
+ *
+ */
+
+package com.rosberry.mpp.jigitbl.data.repositories
+
+import com.rosberry.mpp.delight.SqlDriverFactory
+import com.rosberry.mpp.jigitbl.JigitDatabase
+import com.rosberry.mpp.jigitbl.OwnerQueries
+import com.rosberry.mpp.jigitbl.RepositoryQueries
+import com.rosberry.mpp.jigitbl.data.DatabaseConst
+import com.rosberry.mpp.jigitbl.entity.Repository
+import com.rosberry.mpp.jigitbl.entity.User
+import com.squareup.sqldelight.db.SqlDriver
+
+/**
+ * @author Alexei Korshun on 07/03/2019.
+ */
+class RepositoryDb(
+        sqlDriverFactory: SqlDriverFactory
+) {
+
+    private val sqlDriver: SqlDriver = sqlDriverFactory.create(DatabaseConst.DATABASE_NAME)
+    private val database: JigitDatabase = JigitDatabase(sqlDriver)
+    private val repositoryQueries: RepositoryQueries = database.repositoryQueries
+    private val ownerQueries: OwnerQueries = database.ownerQueries
+
+    fun getAllRepositories(): List<Repository> {
+        return repositoryQueries.selectAll()
+            .executeAsList()
+            .asSequence()
+            .map { repository ->
+                val owner = ownerQueries.selectById(repository.owner_id)
+                    .executeAsOne()
+                Repository(repository.id, repository.name, User(owner.login, owner.id))
+            }
+            .toList()
+    }
+}
